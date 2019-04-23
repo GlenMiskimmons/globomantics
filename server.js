4df00077cb06aef2bed2e59db871360a953fc23f
Patrick Neborg <pluralsightauthor1@outlook.com>
2019-04-23 02:22:15 -0400

    Added express-handlebars

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-var express = require('express'),
-    app = express(),
-     port = process.env.PORT || 80;
-app.use(express.static(__dirname + '/public'));
-app.listen(port, function(){
-    console.log('Server listening on port ', port) 
-}) 
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(80);
