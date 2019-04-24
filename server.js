0258ab9ff1efb93e4f376d0dcefc261575b5e4df
Patrick Neborg <pluralsightauthor1@outlook.com>
2019-04-24 00:08:15 -0400

    Add Globomantics web app

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
+var favicon = require('serve-favicon');
+var routes = require('./routes/index');
 
 const app = express()
 
 app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
+  defaultLayout: 'layout',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
-app.get('/', function (req, res) {
-    res.render('home');
+
+app.use(favicon(path.join(__dirname, 'public', '/Icons/Globo_Icon_Home.png')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+ // error handlers 
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
 });
 
-app.listen(80);
+app.listen(3000);
